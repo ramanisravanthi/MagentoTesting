11a3502bd7444d84aaa5703a93a85da5cb85ca8c
mohammed <mohammed@DESKTOP-O075CBO>
2019-09-21 03:29:23 -0700

    First commit

---
+++ MagentoTesting/src/main/java/Login.java
@@ -0,0 +1,35 @@
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Login 
+{
+	WebDriver driver;
+
+	public Login(WebDriver driver) {
+			this.driver = driver;
+	}
+	By email=By.id("email");
+	By password=By.id("pass");
+	By login=By.id("send2");
+	
+	
+	public void typeEmail()
+	{
+		driver.findElement(email).sendKeys("mzaidurrahman@gmail.com");
+	}
+	public  void typePassword()
+	{
+		driver.findElement(password).sendKeys("Welcome123");
+	}
+	public void clickOnLogin()
+	{
+		driver.findElement(login).click();
+	}
+	
+	
+	
+	
+
+}
